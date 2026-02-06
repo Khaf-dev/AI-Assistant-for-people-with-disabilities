445a21679ccde038a06d2db15927197031793795
Khaf-dev <rifyatkaffa@gmail.com>
2026-02-07 01:05:38 +0700

    new code line, but still uncleared

---
+++ deploy.py
@@ -0,0 +1,11 @@
+import os
+import sys
+import subprocess
+import platform
+
+def setup_environment():
+    """Setup the development environment"""
+    print("Setting up AIForUs environment...")
+    
+    # Create necessary directories
+    
